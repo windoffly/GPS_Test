f9a6e8cc2a0ce6cc4d54fb2fda564f5bca97c103
windoffly <windoffly@gmail.com>
2013-12-04 17:48:36 +0800

    no

--- GPS_Test/ViewController.h
+++ GPS_Test/ViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
+#import <CoreLocation/CoreLocation.h>
+@interface ViewController : UIViewController<
+CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *Location;
+@property (weak, nonatomic) IBOutlet UILabel *adress;
 
 @end
--- GPS_Test/ViewController.m
+++ GPS_Test/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    CLGeocoder*geocder;
+    CLLocationManager*manger;
+}
 
 @end
 
@@ -18,6 +22,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    geocder=[[CLGeocoder alloc]init];
+    manger=[[CLLocationManager alloc]init];
+    manger.delegate=self;
+    manger.desiredAccuracy=kCLLocationAccuracyBestForNavigation;
+    //manger.distanceFilter=kCLDistanceFilterNone;
+    manger.distanceFilter=1;
+    [manger startUpdatingLocation];
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    CLLocation*location =[locations lastObject];
+    self.Location.text=[NSString stringWithFormat:@"%f,%f",location.coordinate.latitude,location.coordinate.longitude];
+    
+[geocder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+    
+   
+    
+    if (!error) {
+        CLPlacemark*mark=[placemarks lastObject];
+        self.adress.text=[NSString stringWithFormat:@"%@%@%@%@",mark.locality,mark.subLocality==nil?@"":mark.subLocality,mark.thoroughfare,mark.subThoroughfare];
+
+    }else{NSLog(@"%@",error);}
+    }];
 }
 
 - (void)didReceiveMemoryWarning
